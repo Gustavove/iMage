2703755edc590cd6750600324b787ecdca204d40
Gustavove <gustavove510@gmail.com>
2022-04-30 18:47:25 +0200

    Gestion partida

--- Server/DataBase/consultasBD.py
+++ Server/DataBase/consultasBD.py
@@ -1,4 +1,5 @@
 import sqlite3
+import random
 from sqlite3 import Error
 import os
 import sys
@@ -17,8 +18,49 @@ def closeBD(conn):
     if conn:
         conn.close()
 
+def consultar_información_partida(conn, codigo_partida):
+    query = "SELECT * FROM partida p"
+    c = conn.cursor()
+    tofilter = []
+    tofilter.append(codigo_partida)
+    result = c.execute(query, tofilter).fetchone()[0]
+    return result
 
+def nueva_partida (conn, rondas_totales):
+    c = conn.cursor()
+    codigo = random.randint(100000, 999999)
+    try:
+        nueva_partida = (codigo, 0, rondas_totales)
+        query = '''INSERT INTO partida (codigo, ronda_actual, rondas_actuales) 
+        VALUES (?,?,?)'''
+        c.execute(query, nueva_partida)
+        conn.commit()
 
+    except Error as e:
+        print(e)
 
+    return codigo
 
+def nuevo_jugador(conn, nick, id_partida):
+    c = conn.cursor()
+    try:
+        nuevo_jugador = (nick, 0, id_partida)
+        query = '''INSERT INTO jugador (nick, puntos, codigo) 
+        VALUES (?,?,?)'''
+        c.execute(query, nuevo_jugador)
+        conn.commit()
+
+    except Error as e:
+        print(e)
+
+def obtener_puntuaciones_totales_partida(conn, id_partida):
+    c = conn.cursor()
+    try:
+        datos = (id_partida)
+        query = ''' SELECT j.nick, j.puntos FROM jugador j WHERE j.partida = ?'''
+        c.execute(query, datos)
+        conn.commit()
+
+    except Error as e:
+        print(e)
 
